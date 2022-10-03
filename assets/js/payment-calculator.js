674a4491eb859b003e2453d6a88403304df21f70
Clint Pitzak <cpitzak@gmail.com>
2022-10-03 14:48:54 -0700

    Updates

--- assets/js/payment-calculator.js
+++ assets/js/payment-calculator.js
@@ -3,5 +3,9 @@ function calculatePayment() {
     const rate = parseFloat(document.getElementById('rate').value);
     const minutes = parseFloat(document.getElementById('minutes').value);
     const result = minutes * (rate / 60.0);
-    document.getElementById('payment-result').innerHTML = '$' + result.toFixed(2);
+    if (isNaN(result)) {
+        document.getElementById('payment-result').innerHTML = 'Invalid input. Please check your entries.';
+    } else {
+        document.getElementById('payment-result').innerHTML = '$' + result.toFixed(2);
+    }
 }
